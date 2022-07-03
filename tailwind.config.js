831bad9d5af2a41bd516e9a62c098a6a8d06a4b2
juuliana <jumaju10@hotmail.com>
2022-07-03 14:41:17 -0300

    fix: config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,14 @@ module.exports = {
     extend: {
       colors: {
         brand: {
+          300: "#996DFF",
           500: "#8257E6",
         },
       },
+      borderRadius: {
+        md: "4px",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms"), require("tailwind-scrollbar")],
 };
